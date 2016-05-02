4da475cd1be02ab1a1ad3da993c47b9ec67ff3fb
Joel Rocha <joelxr@gmail.com>
2016-05-02 05:45:00 -0300

    Alteração para manter a base atualizada e parte do ambiente do artigo

--- src/main/java/br/com/javamagazine/paginacaojm/domain/Cliente.java
+++ src/main/java/br/com/javamagazine/paginacaojm/domain/Cliente.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="cliente")
 public class Cliente {
 
 	@Id
